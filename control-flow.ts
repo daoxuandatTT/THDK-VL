921ae4ba31b8e107abdded9ebbdd158a4a790c49
xuandat.170399@gmail.com <you@example.com>
2019-09-23 11:00:27 +0700

    add file

---
+++ control-flow.ts
@@ -0,0 +1,65 @@
+import "./scss/styles.scss";
+
+/**
+ * If/else
+ */
+/*
+let count = 50;
+if (count > 0) {
+    count--;
+} else {
+    count = 0;
+}
+console.log(count);
+*/
+/**
+ * loop
+ */
+/*
+console.log("for loop");
+
+const keys = "abcdef";
+for (let idx = 0; idx < keys.length; ++idx) {
+    console.log(keys[idx]);
+}
+*/
+/*
+console.log("while loop");
+let idx = 0;
+const keys = '123456';
+while (idx < keys.length) {
+    console.log(keys[idx]);
+    ++idx;
+}
+ */
+/*
+console.log("do-while loop");
+const keys = '1234';
+idx = 0;
+do {
+  console.log(keys[idx]);
+  ++idx;
+} while (idx < keys.length);
+*/
+/*
+console.log("do-while loop");
+const keys = '1234';
+idx = 0;
+do {
+  console.log(keys[idx]);
+  ++idx;
+} while (idx < keys.length);
+for (const item of keys) {
+  console.log(item);
+}
+*/
+/*
+console.log("for-in loop");
+const user = {
+  name: 'Bob',
+  age: 55
+};
+for (const key in user) {
+  console.log(`${key}: ${user[key]}`);
+}
+*/
